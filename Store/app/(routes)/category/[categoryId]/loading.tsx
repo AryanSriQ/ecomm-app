bdccca5c89106e5df3020769982e27d13f708fb4
AryanSriQ <aryan.srivastava@qsstechnosoft.com>
2023-08-02 15:46:00 +0530

    initial commit

---
+++ Store/app/(routes)/category/[categoryId]/loading.tsx
@@ -0,0 +1,34 @@
+import Container from "@/components/ui/container";
+import Skeleton from "@/components/ui/skeleton";
+
+/**
+ * Renders a loading component.
+ *
+ * @return The loading component.
+ */
+const Loading = () => {
+  return (
+    <Container>
+      <div className="w-full h-full p-8">
+        <Skeleton className="w-full aspect-square rounded-xl md:aspect-[2.4/1]" />
+        <div className="h-full mt-8 lg:grid lg:grid-cols-5 lg:gap-x-8">
+          <div className="hidden lg:block">
+            <Skeleton className="w-full h-[500px] rounded-xl" />
+          </div>
+          <div className="mt-6 lg:col-span-4 lg:mt-0">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+              <Skeleton className="aspect-square rounded-xl" />
+              <Skeleton className="aspect-square rounded-xl" />
+              <Skeleton className="aspect-square rounded-xl" />
+              <Skeleton className="aspect-square rounded-xl" />
+              <Skeleton className="aspect-square rounded-xl" />
+              <Skeleton className="aspect-square rounded-xl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+}
+ 
+export default Loading;
